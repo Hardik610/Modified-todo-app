6ea1d3c061e63f74fe76f82e26593e5f2aae052f
hardik610 <hppgupta@gmail.com>
2021-11-28 06:01:56 +0530

    change password length

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def load_user(userID):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.Unicode, nullable=False)
     token = db.Column(db.Unicode, nullable=False)
     date_joined = db.Column(db.DateTime, nullable=False)
